b1fa4ad54f8b6e77f17912f15df20d5aa45b7352
Daria Korniienko <106033233+Dashasoham@users.noreply.github.com>
2023-06-12 12:57:15 +0100

    Updates

---
+++ precache-manifest.a98877daf91642c33a7381b3e7b378e8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "415a814f1a063108e2f2",
+    "url": "/ReduxUsersList/static/css/main.1c6d5baa.chunk.css"
+  },
+  {
+    "revision": "415a814f1a063108e2f2",
+    "url": "/ReduxUsersList/static/js/main.415a814f.chunk.js"
+  },
+  {
+    "revision": "3211eb3683c3a9a41f12",
+    "url": "/ReduxUsersList/static/css/1.2696d69a.chunk.css"
+  },
+  {
+    "revision": "3211eb3683c3a9a41f12",
+    "url": "/ReduxUsersList/static/js/1.3211eb36.chunk.js"
+  },
+  {
+    "revision": "1c26c7bc43f7a905dfdd",
+    "url": "/ReduxUsersList/static/js/runtime~main.1c26c7bc.js"
+  },
+  {
+    "revision": "76587045e4e9990865d5448b68864065",
+    "url": "/ReduxUsersList/index.html"
+  }
+];
